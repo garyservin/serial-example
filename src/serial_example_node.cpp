ee955c27848132f4c0893b6a5cf97643d5790b99
Gary Servin <garyservin@gmail.com>
2014-10-20 23:26:19 -0300

    First commit

---
+++ src/serial_example_node.cpp
@@ -0,0 +1,76 @@
+/***
+ * This example expects the serial port has a loopback on it.
+ *
+ * Alternatively, you could use an Arduino:
+ *
+ * <pre>
+ *  void setup() {
+ *    Serial.begin(<insert your baudrate here>);
+ *  }
+ *
+ *  void loop() {
+ *    if (Serial.available()) {
+ *      Serial.write(Serial.read());
+ *    }
+ *  }
+ * </pre>
+ */
+
+#include <ros/ros.h>
+#include <serial/serial.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Empty.h>
+
+serial::Serial ser;
+
+void write_callback(const std_msgs::String::ConstPtr& msg){
+    ROS_INFO_STREAM("Writing to serial port" << msg->data);
+    ser.write(msg->data);
+}
+
+int main (int argc, char** argv){
+    ros::init(argc, argv, "serial_example_node");
+    ros::NodeHandle nh;
+
+    ros::Subscriber write_sub = nh.subscribe("write", 1000, write_callback);
+    ros::Publisher read_pub = nh.advertise<std_msgs::String>("read", 1000);
+
+    try
+    {
+        ser.setPort("/dev/ttyACM0");
+        ser.setBaudrate(9600);
+        serial::Timeout to = serial::Timeout::simpleTimeout(1000);
+        ser.setTimeout(to);
+        ser.open();
+    }
+    catch (serial::IOException& e)
+    {
+        ROS_ERROR_STREAM("Unable to open port ");
+        return -1;
+    }
+
+    if(ser.isOpen()){
+        ROS_INFO_STREAM("Serial Port initialized");
+    }else{
+        return -1;
+    }
+
+    ros::Rate loop_rate(5);
+    while(ros::ok()){
+
+        ros::spinOnce();
+
+        if(ser.available()){
+            ROS_INFO_STREAM("Reading from serial port");
+            std_msgs::String result;
+            result.data = ser.read(ser.available());
+            ROS_INFO_STREAM("Read: " << result.data);
+            read_pub.publish(result);
+        }
+        loop_rate.sleep();
+
+    }
+
+    ros::spin();
+}
+
